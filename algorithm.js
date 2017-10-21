0e4c9cd4d625ad284c5e06c77212036b9d4849d9
zrajnis <zvone.rajnis@gmail.com>
2017-10-21 18:20:04 +0200

    Implement naive Bayesian classifier.

--- algorithm.js
+++ algorithm.js
@@ -4,9 +4,9 @@ const fs = require('fs')
 const classifier = {
   fc: {},
   cc: {},
+  thresholds: {},
 }
 
-//TODO: naive bayesian classifier
 //TODO: introduce db
 
 classifier.catCount = function (cat) {
@@ -29,6 +29,10 @@ classifier.getFeatures = function (text) {
   const words = _.map(text.match(/[a-z'\-]+/gi), _.toLower)
 
   return words
+}
+
+classifier.getThreshold = function (cat) {
+  return this.thresholds[cat] ? this.thresholds[cat] : 1
 }
 
 /*function getWordsFromDoc (doc) {
@@ -54,6 +58,10 @@ classifier.sampleTrain = function () {
   this.train('the quick brown fox jumps', 'good')
 }
 
+classifier.setThreshold = function (cat, t) {
+  this.thresholds[cat] = t
+}
+
 classifier.totalCount = function () {
   return _.reduce(this.cc, function (result, value) {
     return result + value
@@ -77,16 +85,58 @@ classifier.weightedProb = function (ftr, cat, weight = 1, assumedProb = 0.5) {
   return ((weight * assumedProb) + (totals * basicProb)) / (weight + totals)
 }
 
-classifier.sampleTrain()
-console.log(JSON.stringify(classifier, null, 2))
-console.log('good quick ' + classifier.ftrCount('quick', 'good'))
-console.log('bad quick ' + classifier.ftrCount('quick', 'bad'))
-console.log('probability for good quick ' + classifier.ftrProb('quick', 'good'))
-console.log('weighted probability for good quick ' + classifier.weightedProb('quick', 'good'))
+const naiveBayes = _.assign({}, classifier)
+
+naiveBayes.classify = function (item, def = 'None') {
+  let best = 'None'
+  let max = 0
+  const categories = this.getCategories()
+  const probs = {}
+
+  _.each(categories, function (cat) {
+    probs[cat] = naiveBayes.prob(item, cat)
+
+    if (probs[cat] > max) {
+      max = probs[cat]
+      best = cat
+    }
+  })
+
+  return _.find(_.keys(probs), function (cat) {
+    return cat !== best && probs[cat] * naiveBayes.getThreshold(best) > probs[best]
+  }) ? def : best
+}
+
+naiveBayes.docProb = function (item, cat) {
+  return _.reduce(this.getFeatures(item), function (result, feature) {
+    return result * naiveBayes.weightedProb(feature, cat)
+  }, 1)
+}
+
+naiveBayes.prob = function (item, cat) {
+  const catProb = this.catCount(cat) / this.totalCount()
+  const docProb = this.docProb(item, cat)
+
+  return docProb * catProb
+}
+
+naiveBayes.setThreshold('bad', 3)
+naiveBayes.sampleTrain()
+console.log('threshold for bad is ' + naiveBayes.getThreshold('bad'))
+console.log('good quick ' + naiveBayes.ftrCount('quick', 'good'))
+console.log('bad quick ' + naiveBayes.ftrCount('quick', 'bad'))
+console.log('bayes prob for quick being in good category: ' + naiveBayes.prob('quick', 'good'))
+console.log('bayes prob for quick being in bad category: ' + naiveBayes.prob('quick', 'bad'))
+console.log('classify quick: ' + naiveBayes.classify('quick'))
+console.log('bayes prob for quick money being in good category: ' + naiveBayes.prob('quick money', 'good'))
+console.log('bayes prob for quick money being in bad category: ' + naiveBayes.prob('quick money', 'bad'))
+console.log('classify quick money: ' + naiveBayes.classify('quick money'))
+
 
 module.exports = {
-  ftrProb: classifier.ftrProb,
-  sampleTrain: classifier.sampleTrain,
-  train: classifier.train,
-  weightedProb: classifier.weightedProb,
+  ftrProb: naiveBayes.ftrProb,
+  sampleTrain: naiveBayes.sampleTrain,
+  train: naiveBayes.train,
+  weightedProb: naiveBayes.weightedProb,
+  classify: naiveBayes.classify,
 }
