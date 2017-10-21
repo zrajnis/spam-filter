922ce69f846f23e872ca5c3c062610c6420bb153
zrajnis <zvone.rajnis@gmail.com>
2017-10-21 15:09:48 +0200

    Encapsulate everything into classifier, lint and sort code alphabetically.

--- algorithm.js
+++ algorithm.js
@@ -6,87 +6,87 @@ const classifier = {
   cc: {},
 }
 
-classifier.getFeatures = getWords
-
-//TODO: bind all functions to classifier
 //TODO: naive bayesian classifier
 //TODO: introduce db
 
-function incFtr (ftr,cat) {
-  if (!classifier.fc[ftr]) {
-    classifier.fc[ftr] = {}
-  }
-  classifier.fc[ftr][cat] = classifier.fc[ftr][cat] ? classifier.fc[ftr][cat] + 1 : 1
+classifier.catCount = function (cat) {
+  return this.cc[cat] ? this.cc[cat] : 0
 }
 
-function incCat (cat) {
-  classifier.cc[cat] = classifier.cc[cat] ? classifier.cc[cat] + 1 : 1
+classifier.ftrCount = function (ftr, cat) {
+  return this.fc[ftr] && this.fc[ftr][cat] ? this.fc[ftr][cat] : 0
 }
 
-function ftrCount (ftr, cat) {
-  return classifier.fc[ftr] && classifier.fc[ftr][cat] ? classifier.fc[ftr][cat] : 0
+classifier.ftrProb = function (ftr, cat) {
+  return this.catCount(cat) === 0 ? 0 : this.ftrCount(ftr, cat) / this.catCount(cat)
 }
 
-function catCount (cat) {
-  return classifier.cc[cat] ? classifier.cc[cat] : 0
+classifier.getCategories = function () {
+  return _.keys(this.cc)
 }
 
-function totalCount () {
-  return _.reduce(classifier.cc, function (result, value) {
-    return result + value
-  }, 0)
-}
-
-function getCategories () {
-  return _.keys(classifier.cc)
-}
-
-function getWords (text) {
-  const words = _.map(text.match(/[a-z'\-]+/gi), _.toLower);
+classifier.getFeatures = function (text) {
+  const words = _.map(text.match(/[a-z'\-]+/gi), _.toLower)
 
   return words
 }
 
-function getWordsFromDoc (doc) {
+/*function getWordsFromDoc (doc) {
   const text = fs.readFileSync('./mytext.txt", "utf-8');
+}*/
+
+classifier.incCat = function (cat) {
+  this.cc[cat] = this.cc[cat] ? this.cc[cat] + 1 : 1
 }
 
-function ftrProb (ftr, cat) {
-  return catCount(cat) === 0 ? 0 : ftrCount(ftr, cat) / catCount(cat)
+classifier.incFtr = function (ftr,cat) {
+  if (!this.fc[ftr]) {
+    this.fc[ftr] = {}
+  }
+  this.fc[ftr][cat] = this.fc[ftr][cat] ? this.fc[ftr][cat] + 1 : 1
 }
 
-function weightedProb(ftr, cat, probFn, weight = 1, assumedProb = 0.5) {
-  const basicProb = probFn(ftr, cat)
-  const totals = _.reduce(getCategories(), function (result, category) {
-    return result + ftrCount(ftr, category)
+classifier.sampleTrain = function () {
+  this.train('Nobody owns the water.', 'good')
+  this.train('the quick rabbit jumps fences', 'good')
+  this.train('buy pharmaceuticals now', 'bad')
+  this.train('make quick money at the online casino', 'bad')
+  this.train('the quick brown fox jumps', 'good')
+}
+
+classifier.totalCount = function () {
+  return _.reduce(this.cc, function (result, value) {
+    return result + value
+  }, 0)
+}
+
+classifier.train = function (item, cat) {
+  _.map(this.getFeatures(item), function (feature) {
+    return classifier.incFtr(feature, cat)
+  })
+
+  this.incCat(cat)
+}
+
+classifier.weightedProb = function (ftr, cat, weight = 1, assumedProb = 0.5) {
+  const basicProb = this.ftrProb(ftr, cat)
+  const totals = _.reduce(this.getCategories(), function (result, category) {
+    return result + classifier.ftrCount(ftr, category)
   }, 0)
 
   return ((weight * assumedProb) + (totals * basicProb)) / (weight + totals)
 }
 
-function train (item, cat) {
-  _.map(classifier.getFeatures(item), function (feature) {
-    return incFtr(feature, cat)
-  })
-
-  incCat(cat)
-}
-
-function sampleTrain () {
-  train('Nobody owns the water.', 'good')
-  train('the quick rabbit jumps fences', 'good')
-  train('buy pharmaceuticals now', 'bad')
-  train('make quick money at the online casino', 'bad')
-  train('the quick brown fox jumps', 'good')
-}
-
-sampleTrain();
+classifier.sampleTrain()
 console.log(JSON.stringify(classifier, null, 2))
-console.log('good quick ' + ftrCount('quick', 'good'))
-console.log('bad quick ' + ftrCount('quick', 'bad'))
-console.log('probability for good quick ' + ftrProb('quick', 'good'))
-console.log('weighted probability for good quick ' + weightedProb('quick', 'good', ftrProb))
+console.log('good quick ' + classifier.ftrCount('quick', 'good'))
+console.log('bad quick ' + classifier.ftrCount('quick', 'bad'))
+console.log('probability for good quick ' + classifier.ftrProb('quick', 'good'))
+console.log('weighted probability for good quick ' + classifier.weightedProb('quick', 'good'))
 
 module.exports = {
-  getWords
+  ftrProb: classifier.ftrProb,
+  sampleTrain: classifier.sampleTrain,
+  train: classifier.train,
+  weightedProb: classifier.weightedProb,
 }
